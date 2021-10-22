a14e5f04f0ba85618fec6367d39760af63e6855c
Kian <kezaz@ucsd.edu>
2021-10-22 15:39:47 -0700

    Added voice options

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,31 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  let voices = []; // global array
+
+  window.speechSynthesis.onvoiceschanged = () => {
+    // Get List of Voices
+    voices = window.speechSynthesis.getVoices();
+    const voiceSelection = document.querySelector('select');
+    for (let i = 0; i < voices.length; i++) {
+      const voiceOption = document.createElement('option');
+      voiceOption.text = voices[i].name;
+      voiceSelection.add(voiceOption);
+    }
+  }
+
+  const speakButton = document.querySelector('button');
+
+  speakButton.addEventListener('click', () => {
+    const text = document.getElementById('text-to-speak').value;
+    let utterance = new SpeechSynthesisUtterance(text)
+    const selectedVoice = document.querySelector('select').value;
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedVoice) {
+        utterance.voice = voices[i];
+      }
+    }
+    speechSynthesis.speak(utterance);
+  });
 }
